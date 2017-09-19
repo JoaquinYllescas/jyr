632d9cb577d4746c73228ffd99b6c546c5dc1086
JoaquinYllescas <jyllescasruiz@gmail.com>
2017-09-19 17:35:17 +0100

    Working in Home Page

--- JYR/template-jyr.php
+++ JYR/template-jyr.php
@@ -1,12 +1,32 @@
-<?php /* Template Name: JYR Template */ get_header(); ?>
+<?php /* Template Name: JYR Template Home */ get_header(); ?>
+<div class="data">
+<?php
+$fields = get_fields();
+print_r($fields);
+//echo get_theme_root_uri();
+
+?> 
+</div>
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="main" style="background-image:url('<?php echo($fields["background"]["url"]); ?>');">
 
-			<h1><?php the_title(); ?></h1>
+			<!-- <h1><?php the_title(); ?></h1> -->
 
-		
+			<p>
+				<?php
+					
+
+				?> 
+			</p>
+			<br/>
+			<br/>
+			<br/>
+			<p>
+				
+			</p>
+
 
 		</section>
 		<!-- /section -->
