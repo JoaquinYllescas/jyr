e0f824e85d57f4fcdc55a6df7c930f472f2c39ef
Joaquin Yllescas <joaquinyllescas@MacBook-Pro-de-Joaquin.local>
2018-03-14 19:35:35 +0100

    Subiendo mierda

--- themes/JYR/template-jyr-photography.php
+++ themes/JYR/template-jyr-photography.php
@@ -3,17 +3,60 @@
 <?php
 $fields = get_fields();
 print_r($fields);
-//echo get_theme_root_uri();
 
+
+
+$posts = get_posts(array(
+	'numberposts'	=> -1,
+	'post_type'		=> 'photo_project'
+));
+print_r($posts);
 ?> 
 </div>
 
-	<main role="main">
-		
-		<h1>
-			<?php echo(get_the_title()); ?>
-		</h1>
-	</main>
+	<?php
+		if(($fields["video/img"] == "Image") &&  ($fields["image_source"] != "") ){
+	?>
+			<main class="photography" role="main" style="background-image:url('<?php echo $fields["image_source"] ?>');">
+	<?php 
+		}elseif( ($fields["video/img"] == "Video")  &&  ($fields["video_source"] != "") ){
+	?>
+			<main class="photography" role="main">
+				<video id="bgvid_photography" autoplay muted loop>
+			    	<source src="<?php echo $fields["video_source"]  ?>" type="video/mp4">
+			    </video>
+
+    <?php 
+    	}
+    ?>
+
+				<h1>
+					<?php echo(get_the_title()); ?>
+				</h1>
+				
+				<div class="projects">
+					<!-- the loop -->
+					<?php
+					if( $posts ): ?>
+						<ul>
+							<?php foreach( $posts as $post ): ?>
+								<li>
+									<h2>
+										<?php
+											// print_r($post);
+											echo $post->post_title;
+										?>
+									</h2>
+									<img src="<?php echo (get_field( "background_project", $post->ID ));  ?>" alt="">
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					<?php endif; ?>
+					<!-- end of the loop -->
+				</div>
+				
+			</main>
+
 
 
 <?php
